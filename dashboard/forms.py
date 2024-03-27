a63326da73c9cc03f39462c818d7a00403f7407e
codeedexbackend <159438131+codeedexbackend@users.noreply.github.com>
2024-03-27 15:29:40 +0530

    Delete dashboard directory

--- dashboard/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import AddTailors
-
-class TailorForm(forms.ModelForm):
-    class Meta:
-        model = AddTailors
-        fields = ['tailor','username','password','mobile_number']
